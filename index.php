f428260e48bc849cb96943f3508b89534ac906d7
Muhammad Dicky Hadiyansyah <kiky8894@gmail.com>
2021-04-04 20:29:34 +0700

    menambahkan h2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote lagi</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Non harum inventore commodi voluptatem pariatur? Sapiente,
+     adipisci quidem recusandae exercitationem totam quae ab, modi impedit,
+     nihil sit cupiditate temporibus deleniti veritatis.</p>
+    <h1>hello git</h1>
+    <h2>git CLI</h2>
+</body>
+</html>
